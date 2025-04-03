8e5baa5635ee776ec14bf9cab990b09c108a2f11
prakashrajan <prakashrajan2908@gmail.com>
2025-04-03 15:24:02 +0530

    file change

--- frontend/src/pages/APIFetch.tsx
+++ frontend/src/pages/APIFetch.tsx
@@ -1,27 +1,27 @@
-import axios from 'axios'
-import { useEffect } from 'react'
+// import axios from 'axios'
+// import { useEffect } from 'react'
 
 
 
-const Data = () => {
-    useEffect(() => {
-        respo()
-    }, [])
-        const token = 'github_pat_11BEWZ6LA0WvUC8YVUvue1_n03oCGdN8dmO2z0uIIotzUoC401I8uQFQaI2rukybNdBVLZJELMCrtesTeD'
-    const respo = async () => {
-       const res = await axios.get('https://api.github.com/organizations',
-            {
-                headers:{Authorization :`Bearer ${token}`}
-              }
-        )
-        console.log(res);
-    }
-    return (
-        <div>
-            hii
-        </div>
-    )
+// const Data = () => {
+//     useEffect(() => {
+//         respo()
+//     }, [])
+//     const respo = async () => {
+                    // put token here
+//        const res = await axios.get('https://api.github.com/organizations',
+//             {
+//                 headers:{Authorization :`Bearer ${token}`}
+//               }
+//         )
+//         console.log(res);
+//     }
+//     return (
+//         <div>
+//             hii
+//         </div>
+//     )
     
-}
+// }
 
-export default Data
+// export default Data
